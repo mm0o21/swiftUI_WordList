aeb35477e3c9e3eb8ea34b6ee91e1bc1e16c4131
mm0o21 <maaaoko0021@gmail.com>
2025-06-18 00:33:57 +0900

    kansei

---
+++ WordList/AddWordView.swift
@@ -0,0 +1,18 @@
+//
+//  AddWordView.swift
+//  WordList
+//
+//  Created by maoko furuya on 2025/06/17.
+//
+
+import SwiftUI
+
+struct AddWordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddWordView()
+}
